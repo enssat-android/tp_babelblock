1122d3fc6591283e6a8a92cd0eedbb7f16e60e23
gilles le brun <gil.lebrun@orange.com>
2020-11-27 11:27:00 +0100

    feature: show how to delete/reload mlkit model from a given source language

--- app/src/main/java/fr/enssat/babelblock/tools/impl/TranslatorHandler.kt
+++ app/src/main/java/fr/enssat/babelblock/tools/impl/TranslatorHandler.kt
@@ -3,6 +3,8 @@ package fr.enssat.babelblock.tools.impl
 import android.content.Context
 import android.util.Log
 import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.common.model.RemoteModelManager
+import com.google.mlkit.nl.translate.TranslateRemoteModel
 import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.TranslatorOptions
 import fr.enssat.babelblock.tools.TranslationTool
@@ -21,9 +23,19 @@ class TranslatorHandler(context: Context, from: Locale, to: Locale): Translation
                                 .requireWifi()
                                 .build()
 
+    private val modelManager = RemoteModelManager.getInstance()
+
+    private val model =  TranslateRemoteModel.Builder(from.toString()).build()
+
     init {  translator.downloadModelIfNeeded(conditions)
                 .addOnSuccessListener { Log.d("Translation", "download completed") }
-                .addOnFailureListener { e -> Log.e("Translation", "Download failed ", e) }
+                .addOnFailureListener {
+                    e -> Log.e("Translation", "Download failed ", e)
+                    //remove any previous model downloaded
+                    modelManager.deleteDownloadedModel(model)
+                    //retry
+                    modelManager.download(model,conditions)
+                }
     }
 
     override fun translate(text: String, callback: (String) -> Unit) {
